baf1d2cfd42ca359c80fad7b7f85773cf04faa90
ao.lebedev <ao.lebedev@tensor.ru>
2016-11-14 17:56:13 +0300

    Очередной виток

--- cutImage/index.js
+++ cutImage/index.js
@@ -80,6 +80,63 @@ CutImage.prototype._subscribeOnMouseEvent = function() {
           self._mainDiv.removeEventListener('mousemove', mouseMove);
           self._mainDiv.removeEventListener('mouseup', mouseUp);
        },
+       mouseMoveBottomRight = function(event) {
+          var
+              rectangleLeftOffset =  parseInt(self._rectangle.style.left.substr(0, self._rectangle.style.left.length - 2)),
+              rectangleWidth,
+              cursorOffset = self._screenToOffset(self._mainDiv, event);
+
+          lastMouseX = event.offsetX;
+          rectangleWidth = Math.abs(cursorOffset.x - rectangleLeftOffset);
+          self._rectangle.style.height = ((rectangleWidth * self._height) / self._width).toFixed() + 'px';
+          self._rectangle.style.width = rectangleWidth + 'px';
+       },
+       mouseUpBottomRight = function(event) {
+          self._mainDiv.removeEventListener('mousemove', mouseMoveBottomRight);
+          self._mainDiv.removeEventListener('mouseup', mouseUpBottomRight);
+       },
+       mouseMoveBottomLeft = function(event) {
+          var
+              rectangleTopOffset =  self._parseStyle(self._rectangle.style.top),
+              rectangleLeftOffset = self._parseStyle(self._rectangle.style.left),
+              rectangleHeight,
+              cursorOffset = self._screenToOffset(self._mainDiv, event),
+              oldWidth = self._parseStyle(self._rectangle.style.width);
+
+          lastMouseX = event.offsetX;
+          rectangleHeight = Math.abs(cursorOffset.y - rectangleTopOffset);
+          self._rectangle.style.width = ((rectangleHeight * self._width) / self._height).toFixed() + 'px';
+          self._rectangle.style.height = rectangleHeight + 'px';
+          self._rectangle.style.left = (rectangleLeftOffset - (self._parseStyle(self._rectangle.style.width) - oldWidth)) + 'px';
+       },
+       mouseUpBottomLeft = function(event) {
+          self._mainDiv.removeEventListener('mousemove', mouseMoveBottomLeft);
+          self._mainDiv.removeEventListener('mouseup', mouseUpBottomLeft);
+       },
+       mouseMoveTopRight = function(event) {
+          var
+              rectangleLeftOffset =  self._parseStyle(self._rectangle.style.left),
+              rectangleTopOffset = self._parseStyle(self._rectangle.style.top),
+              rectangleWidth,
+              cursorOffset = self._screenToOffset(self._mainDiv, event),
+              oldHeight = self._parseStyle(self._rectangle.style.height);
+
+          lastMouseX = event.offsetX;
+          rectangleWidth = Math.abs(cursorOffset.x - rectangleLeftOffset);
+          self._rectangle.style.height = ((rectangleWidth * self._height) / self._width).toFixed() + 'px';
+          self._rectangle.style.width = rectangleWidth + 'px';
+          self._rectangle.style.top = (rectangleTopOffset - (self._parseStyle(self._rectangle.style.height) - oldHeight)) + 'px';
+       },
+       mouseUpTopRight = function(event) {
+          self._mainDiv.removeEventListener('mousemove', mouseMoveTopRight);
+          self._mainDiv.removeEventListener('mouseup', mouseUpTopRight);
+       },
+       mouseMoveTopLeft = function(event) {
+
+       },
+       mouseUpTopLeft = function(event) {
+
+       },
        stop = function() {
           return false;
        };
@@ -93,8 +150,21 @@ CutImage.prototype._subscribeOnMouseEvent = function() {
       self._mainDiv.addEventListener('mousemove', mouseMove);
       self._mainDiv.addEventListener('mouseup', mouseUp);
    });
-
-
+   this._bottomRightResize.addEventListener('mousedown', function(event) {
+      event.stopPropagation();
+      self._mainDiv.addEventListener('mousemove', mouseMoveBottomRight);
+      self._mainDiv.addEventListener('mouseup', mouseUpBottomRight);
+   });
+   this._bottomLeftResize.addEventListener('mousedown', function(event) {
+      event.stopPropagation();
+      self._mainDiv.addEventListener('mousemove', mouseMoveBottomLeft);
+      self._mainDiv.addEventListener('mouseup', mouseUpBottomLeft);
+   });
+   this._topRightResize.addEventListener('mousedown', function(event) {
+      event.stopPropagation();
+      self._mainDiv.addEventListener('mousemove', mouseMoveTopRight);
+      self._mainDiv.addEventListener('mouseup', mouseUpTopRight);
+   });
 
    this._mainDiv.ondragstart = stop;
    this._topLeftResize.ondragstart = stop;
@@ -107,7 +177,11 @@ CutImage.prototype._screenToOffset = function(elem, event) {
    var elemScreenCoordinates = elem.getBoundingClientRect();
 
    return {
-      x: event.screenX - elemScreenCoordinates.left,
-      y: event.screenY - elemScreenCoordinates.top
+      x: event.pageX - elemScreenCoordinates.left,
+      y: event.pageY - elemScreenCoordinates.top
    };
-}
+};
+
+CutImage.prototype._parseStyle = function(styleString) {
+   return parseInt(styleString.substr(0, styleString.length - 2));
+};
